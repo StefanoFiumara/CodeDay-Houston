67c102510eab5d568766d402cf56d3054a835a96
Stefano Fiumara <sdf10517@gmail.com>
2014-11-08 22:43:49 -0600

    Finding closest enemy

--- AssaultCubeAimbot/AssaultCubeAimbot/MainForm.cs
+++ AssaultCubeAimbot/AssaultCubeAimbot/MainForm.cs
@@ -49,19 +49,125 @@ namespace AssaultCubeAimbot
             if (GameFound)
             {
                 int playerBase = MemReader.ReadMultiLevelPointer(MainPlayer.baseAddress, 4, MainPlayer.multilevel);
-                PlayerPositionXLabel.Text = "X: " + MemReader.ReadFloat(playerBase + MainPlayer.offsets.xPos);
-                PlayerPositionYLabel.Text = "Y: " + MemReader.ReadFloat(playerBase + MainPlayer.offsets.yPos);
-                PlayerPositionZLabel.Text = "Z: " + MemReader.ReadFloat(playerBase + MainPlayer.offsets.zPos);
+                UpdateLabels(playerBase);
 
                 int hotkey = ProcessMemoryReaderApi.GetKeyState(02); //Right mouse button
                 if ((hotkey & 0x8000) != 0)
                 {
                     FocusingOnEnemy = true;
+                    Aimbot();
+                }
+                else
+                {
+                    FocusingOnEnemy = false;
+                    FocusTarget = -1;
+                }
+            }
+            try
+            {
+                if (MyProcesses != null)
+                {
+                    if (MyProcesses[0].HasExited)
+                    {
+                        GameFound = false;
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("There was an error: " + ex.Message);
+            }
+        }
+
+        private void UpdateLabels(int playerBase)
+        {
+            PlayerPositionXLabel.Text = "X: " + MemReader.ReadFloat(playerBase + MainPlayer.offsets.xPos);
+            PlayerPositionYLabel.Text = "Y: " + MemReader.ReadFloat(playerBase + MainPlayer.offsets.yPos);
+            PlayerPositionZLabel.Text = "Z: " + MemReader.ReadFloat(playerBase + MainPlayer.offsets.zPos);
+        }
+
+        private void Aimbot()
+        {
+            PlayerDataValues pDataValues = GetPlayerData(MainPlayer);
+            List<PlayerDataValues> EnemyDataValues = new List<PlayerDataValues>();
+
+            for (int i = 0; i < EnemyAddresses.Count; i++)
+            {
+                PlayerDataValues enemyData = GetPlayerData(EnemyAddresses[i]);
+                EnemyDataValues.Add(enemyData);
+            }
+
+            if (pDataValues.health > 0)
+            {
+                int target = 0;
+                if (FocusingOnEnemy && FocusTarget != -1)
+                {
+                    if (EnemyDataValues[FocusTarget].health > 0)
+                    {
+                        target = FocusTarget;
+                    }
+                    else
+                    {
+                        target = FindClosestEnemyIndex();
+                    }
                 }
             }
         }
 
-        
+        private int FindClosestEnemyIndex(PlayerDataValues[] enemies, PlayerDataValues myPosition) 
+        {
+            float[] distances = new float[enemies.Length];
+            //fill array with all enemy distances
+            for (int i = 0; i < enemies.Length; i++)
+            {
+                if (enemies[i].health > 0)
+                {
+                    distances[i] = Get3DDistance(enemies[i], myPosition);
+                }
+                else
+                {
+                    distances[i] = float.MaxValue;
+                }
+            }
+            
+            //create new array, copy the values and sort them.
+            float[] sortedDistances = new float[distances.Length];
+            Array.Copy(distances, sortedDistances, distances.Length);
+            Array.Sort(sortedDistances);
+
+            //Find the minimum value and return the index of it.
+
+            //find the index of the smallest distance and return it.
+            for (int i = 0; i < distances.Length; i++)
+            {
+                if (distances[i] == sortedDistances[0])
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        private float Get3DDistance(PlayerDataValues to, PlayerDataValues from)
+        {
+            return (float)(Math.Sqrt(
+                                     ((to.xPos - from.xPos) * (to.xPos - from.xPos)) +
+                                     ((to.yPos - from.yPos) * (to.yPos - from.yPos)) +
+                                     ((to.zPos - from.zPos) * (to.zPos - from.zPos))));
+        }
+        private PlayerDataValues GetPlayerData(PlayerData player)
+        {
+            PlayerDataValues pDataValues = new PlayerDataValues();
+            int playerBase = MemReader.ReadMultiLevelPointer(player.baseAddress, 4, MainPlayer.multilevel);
+            pDataValues.xMouse = MemReader.ReadFloat(playerBase + player.offsets.xMouse);
+            pDataValues.yMouse = MemReader.ReadFloat(playerBase + player.offsets.yMouse);
+            pDataValues.xPos = MemReader.ReadFloat(playerBase + player.offsets.xPos);
+            pDataValues.yPos = MemReader.ReadFloat(playerBase + player.offsets.yPos);
+            pDataValues.zPos = MemReader.ReadFloat(playerBase + player.offsets.zPos);
+            pDataValues.health = MemReader.ReadInt(playerBase + player.offsets.health);
+            return pDataValues;
+        }
+
         private void FindProcessButton_Click(object sender, EventArgs e)
         {
             try
